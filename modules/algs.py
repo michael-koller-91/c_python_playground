fceb4520507772f8a928c2fcc48b30f95a4f74ad
Michael Koller <koller_michael@outlook.de>
2020-09-29 19:51:06 +0200

    update algs.py: add power_iteration_jit

--- modules/algs.py
+++ modules/algs.py
@@ -8,7 +8,7 @@ def omp(A, y, s):
         \min_x \| A x - y \|
     for a s-sparse x.
     Args:
-        A: A m-by-n matrix (typically m < n).
+        A: An m-by-n matrix (typically m < n).
         y: An m-dimensional right-hand side vector.
         s: The sparsity (number of non-zero entires of x)
 
@@ -35,7 +35,7 @@ def omp_jit(A, y, s):
         \min_x \| A x - y \|
     for a s-sparse x.
     Args:
-        A: A m-by-n matrix (typically m < n).
+        A: An m-by-n matrix (typically m < n).
         y: An m-dimensional right-hand side vector.
         s: The sparsity (number of non-zero entires of x)
 
@@ -76,13 +76,52 @@ def power_iteration(A, v_init, eps=1e-12, max_iterations=100):
     A = (A.conj().T + A) * 0.5
 
     mu = 0.0
-    mu_previous = 0
+    mu_previous = 0.0
     for _ in range(max_iterations):
-        w = A @ v_init
-        v_init = w / np.linalg.norm(w)
+        v = A @ v_init
+        v_init = v / np.linalg.norm(v)
 
         # Rayleigh quotient
         mu = v_init.conj().T @ A @ v_init
+
+        # check relative change in eigenvalue
+        if np.abs((mu - mu_previous) / mu) <= eps:
+            break
+
+        mu_previous = mu
+    return np.real(mu), v_init
+
+
+@numba.njit
+def power_iteration_jit(A, v_init, eps=1e-12, max_iterations=100):
+    """
+    Find the largest (in absolute value) eigenvalue and corresponding
+    eigenvector of a square symmetric/self-adjoint matrix A using the
+    initialization vector v_init. The algorithm converges if the maximum number
+    of iterations, max_iterations, is reached or if the relative change of the
+    estimated eigenvalue from one iteration to the next is smaller than or
+    equal to eps.
+
+    Returns:
+        mu: The largest eigenvalue.
+        v: The corresponding n-dimensional eigenvector.
+    """
+    # square matrix
+    assert A.shape[0] == A.shape[1]
+    assert A.shape[0] == v_init.shape[0]
+
+    # make sure A is symmetric/self-adjoint
+    A = (A.conj().T + A) * 0.5
+
+    mu = 0.0
+    mu_previous = 0.0
+    Av = A @ v_init
+    for _ in range(max_iterations):
+        v_init = Av / np.linalg.norm(Av)
+
+        # Rayleigh quotient
+        Av = A @ v_init
+        mu = v_init.conj().T @ Av
 
         # check relative change in eigenvalue
         if np.abs((mu - mu_previous) / mu) <= eps:
